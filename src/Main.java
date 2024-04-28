3d3ba104e93c69733ab9645ff36f77913614d2fa
John Efren Gannaban <jefjef1412@gmail.com>
2024-04-28 12:23:45 +0800

    updates

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
                 //System.out.println(WeatherApp.getLocationData("Manila"));
 
-               // System.out.println(WeatherApp.getCurrentTime());
+                System.out.println(WeatherApp.getCurrentTime());
             }
         });
     }
